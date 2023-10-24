063e93b921d3a6b2d3932bae770b844b64aef133
MaxBenen <max_benen@hotmail.com>
2023-10-24 17:11:15 +0200

    Initial Commit

---
+++ Doti/DotiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DotiApp.swift
+//  Doti
+//
+//  Created by Max Benenn on 24/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct DotiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
