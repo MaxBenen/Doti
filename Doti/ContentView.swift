bb8e6f5612a4de58c3c379b9c590c468368c8290
MaxBenen <max_benen@hotmail.com>
2023-10-25 13:00:29 +0200

    first commit

--- Doti/ContentView.swift
+++ Doti/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        @State var TextfieldText: String = ""
+        
+        TextField("Type your todo", text: $TextfieldText)
+            //.textFieldStyle(RoundedBorderTextFieldStyle())
+            .padding()
+            .background(Color.gray .opacity(0.2).cornerRadius(10))
+            .padding(30)
+        
     }
 }
 
