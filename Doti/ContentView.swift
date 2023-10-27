5f811936e3a0245cc465fdb98db3cdef852a8002
MaxBenen <max_benen@hotmail.com>
2023-10-27 12:26:51 +0200

    Added alert

--- Doti/ContentView.swift
+++ Doti/ContentView.swift
@@ -8,18 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var TextfieldText: String = ""
+    @State var showAlert: Bool = false
+
     var body: some View {
         
-        @State var TextfieldText: String = ""
+
+//        TextField("Type your todo", text: $TextfieldText)
+//            //.textFieldStyle(RoundedBorderTextFieldStyle())
+//            .padding()
+//            .background(Color.gray .opacity(0.2).cornerRadius(10))
+//            .padding(30)
         
-        TextField("Type your todo", text: $TextfieldText)
-            //.textFieldStyle(RoundedBorderTextFieldStyle())
-            .padding()
-            .background(Color.gray .opacity(0.2).cornerRadius(10))
-            .padding(30)
+        Button("Add"){
+            showAlert.toggle()
+        }
         
-    }
-}
+        .alert(isPresented: $showAlert, content: {
+            Alert(
+                title: Text("Add your todo"),
+                primaryButton: .default(Text("Add")),
+                //call action like normal button
+                secondaryButton: .destructive(Text("Cancel")))
+            //Alert(title: Text("Add your todo"))
+        })
+        
+
+        
+    }//body
+    
+    func printing (){
+    
+    }//func printing
+    
+}//contentview
 
 #Preview {
     ContentView()
