19018b863ef5687e16f4367610e287e826b3f6a7
MaxBenen <max_benen@hotmail.com>
2023-11-02 11:14:11 +0100

    Added alert for adding to do

--- Doti/ContentView.swift
+++ Doti/ContentView.swift
@@ -11,36 +11,47 @@ struct ContentView: View {
     
     @State var TextfieldText: String = ""
     @State var showAlert: Bool = false
+    @State var dataArray: [String] = []
 
     var body: some View {
         
 
-//        TextField("Type your todo", text: $TextfieldText)
-//            //.textFieldStyle(RoundedBorderTextFieldStyle())
+//            TextField("Type your todo", text: $TextfieldText)
+//            .textFieldStyle(RoundedBorderTextFieldStyle())
 //            .padding()
 //            .background(Color.gray .opacity(0.2).cornerRadius(10))
 //            .padding(30)
         
-        Button("Add"){
-            showAlert.toggle()
+        Button("Add"){//Centerd in right corner
+            showAlert = true
+        }
+        Spacer()
+        
+        
+        .alert("Add to do", isPresented: $showAlert){
+            TextField(TextfieldText, text: $TextfieldText)
+            Button("Save",action: {})//Action apppend to list and show on screen vertical stacked
+            Button("Cancel",action: {})//Close alert
         }
         
-        .alert(isPresented: $showAlert, content: {
-            Alert(
-                title: Text("Add your todo"),
-                primaryButton: .default(Text("Add")),
-                //call action like normal button
-                secondaryButton: .destructive(Text("Cancel")))
-            //Alert(title: Text("Add your todo"))
-        })
+//        .alert(isPresented: $showAlert, content: {
+//            Alert(
+//                title: Text("Add your todo"),
+//                primaryButton: .default(Text("Add")),
+//                //call action like normal button
+//                secondaryButton: .destructive(Text("Cancel")))
+//           
+//        })
         
 
         
     }//body
     
-    func printing (){
+    func saveText(){
+        dataArray.append(TextfieldText)
+        
+    }//end function save text 
     
-    }//func printing
     
 }//contentview
 
