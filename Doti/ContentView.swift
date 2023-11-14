f18148aa6ed12b6f8dc2b1ad66e5000a317838dc
MaxBenen <max_benen@hotmail.com>
2023-11-14 12:48:15 +0100

    Added list with toolbat (Add button)

--- Doti/ContentView.swift
+++ Doti/ContentView.swift
@@ -11,61 +11,96 @@ struct ContentView: View {
     
     @State var TextfieldText: String = ""
     @State var showAlert: Bool = false
-    //@State var dataArray: [String] = []
+    
+    @State var dataArray: [toDoModel] = [
+        toDoModel(toDoTitle: "Hello", ToDoDone: false),
+        toDoModel(toDoTitle: "Nice", ToDoDone: true)]
+    
     @State var checkBox: Bool = true
-    var toDoList:[(name:String,checked:Bool)] = []
     
     
+    
+    struct toDoModel: Identifiable{
+        let id: String = UUID().uuidString
+        let toDoTitle: String
+        let ToDoDone: Bool
+    }
     
 
     var body: some View {
         
-                
-                
+       
+
+ 
+       
+        
+//                Button("Add"){
+//                    showAlert = true
+//                }
+//        
+//
+//                .alert("Add to do", isPresented: $showAlert){
+//                    
+//                    TextField(TextfieldText, text: $TextfieldText)
+//                    
+//                    Button("Save",action: { })//Action apppend to  list and show on screen vertical stacked
+//                    Button("Cancel",action: {TextfieldText = ""})//Close alert
+//                }
+   
+        
+        NavigationView{
+ 
             
+            List{
                 
-                Button("Add"){
+                ForEach(dataArray){ data in
+                    
+                    Text("byi")
+                }//end loop
+                
+                
+                .navigationTitle("Todo list")
+                
+               
+                
+            }// end list
+            
+            .toolbar {
+                Button("Add") {
                     showAlert = true
                 }
-        
-
                 .alert("Add to do", isPresented: $showAlert){
                     
                     TextField(TextfieldText, text: $TextfieldText)
                     
-                    Button("Save",action: {saveToDo() })//Action apppend to list and show on screen vertical stacked
+                    Button("Save",action: { })//Action apppend to  list and show on screen vertical stacked
                     Button("Cancel",action: {TextfieldText = ""})//Close alert
                 }
-            
-                
-            
-//            ForEach(dataArray, id: \.self){ data in
-//                
-//                    Toggle(data , isOn: $checkBox )
-//                    
-//    
-//            }// loop for saving todos
-        
+            } //end toolbar
+
+        }//nav view
       
 
       
             
     }//body
     
-    func saveToDo(){
-        toDoList.append(TextfieldText)
-        TextfieldText = ""
-    }
+//    func saveToDo(){
+//        toDoList.append ((name: "Anything",checked: true))
+//        TextfieldText = ""
+//    }
+  
+    
     
 //    func saveToDo(){
 //        dataArray.append(TextfieldText)
 //        TextfieldText = ""
-//        
-//    }//end function save text
+        
+    }//end function save text
     
     
     
-}//contentview
+//contentview
 
 #Preview {
     ContentView()
